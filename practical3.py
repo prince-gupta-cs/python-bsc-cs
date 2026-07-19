2bd0daacae32d8b358ff499f7be84b6d27e4b2cb
Prince H Gupta <guptaprince9317@gmail.com>
2026-07-19 20:11:04 +0530

    Create practical3.py

---
+++ practical3.py
@@ -0,0 +1,10 @@
+# ****CALCULATOR****
+
+a=9
+b=7
+print("The Value Of" ,a, "+",b, "is:" ,a+b)
+print("The Value Of" ,a, "-",b, "is:" ,a-b)
+print("The Value Of" ,a, "*",b, "is:" ,a*b)
+print("The Value Of" ,a, "/",b, "is:" ,a/b)
+print("The Value Of" ,a, "//",b, "is:" ,a//b)
+print("The Value Of" ,a, "**",b, "is:" ,a**b) 
