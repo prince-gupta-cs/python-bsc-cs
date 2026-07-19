a9bfb735ff5c30afef3ddd7fe8c728c2d3d3389d
Prince H Gupta <guptaprince9317@gmail.com>
2026-07-19 10:44:54 +0530

    Add files via upload

---
+++ program.py
@@ -0,0 +1,26 @@
+def calculateGmean(a, b):
+    gmean = (a * b) / (a + b)
+    print(f"GMean of {a} and {b} is: {gmean}")
+
+def isGreater(a, b):
+    if (a > b):
+        print("First number is greater")
+    else:
+        print("Second number is greater or equal")
+
+def isLesser(a, b):
+    if (a < b):
+        print("First number is lesser")
+    else:
+        print("Second number is lesser or equal")
+
+# Main code
+a = 9
+b = 8
+is Greater(a, b)
+calculate Gmean (a, b)
+
+c = 8
+d = 74
+is Greater(c, d)
+calculate Gmean (c, d)
