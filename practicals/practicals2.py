7ebcdd240127805a37e2ac05a42f492b9eab5391
Prince H Gupta <guptaprince9317@gmail.com>
2026-07-19 18:06:31 +0530

    Create practicals2.py

---
+++ practicals/practicals2.py
@@ -0,0 +1,8 @@
+# Practical 2: Check Even or Odd
+# Date: 20 July 2026
+
+num = int(input("Ek number daalo: "))
+if num % 2 == 0:
+    print(num, "Even number hai")
+else:
+    print(num, "Odd number hai") 
